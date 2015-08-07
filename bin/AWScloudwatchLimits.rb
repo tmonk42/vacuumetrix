296be2dbc845d93335b04a0d224d8f455d53d721
Haneysmith, Nathan <Nathan.Haneysmith@nordstrom.com>
2015-08-07 14:54:32 -0700

    First batch of AWS account limits updates

--- bin/AWScloudwatchLimits.rb
+++ bin/AWScloudwatchLimits.rb
@@ -13,6 +13,7 @@ require 'fog'
 require 'json'
 require 'aws-sdk'
 require 'optparse'
+require 'hand-tracked-aws-limits'
 
 $options = {
     :start_offset => 180,
@@ -54,8 +55,46 @@ autoscaling_sdk = Aws::AutoScaling::Client.new(region:$awsregion, credentials:cr
 ec2_sdk = Aws::EC2::Client.new(region:$awsregion, credentials:creds)
 autoscaling = Fog::AWS::AutoScaling.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey, :region => $awsregion)
 compute = Fog::Compute.new(:provider => :aws, :aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey, :region => $awsregion)
+cloudformation = Fog::AWS::CloudFormation.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey, :region => $awsregion)
+elasticbeanstalk = Fog::AWS::ElasticBeanstalk.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey, :region => $awsregion)
 
 account_limits = {}
+account_values = {}
+
+# Only query these "hardcoded" limits if the max values appear in the configs
+if $eb_apps
+  account_limits['eb_apps'] = $eb_apps
+  applications = elasticbeanstalk.applications.all
+  account_values['eb_apps'] = applications.length
+end
+if $eb_versions
+  account_limits['eb_versions'] = $eb_versions
+  versions = elasticbeanstalk.versions.all
+  account_values['eb_versions'] = versions.length
+end
+if $eb_envs
+  account_limits['eb_envs'] = $eb_envs
+  environments = elasticbeanstalk.environments.all
+  account_values['eb_envs'] = environments.length
+end
+if $eb_templates
+  account_limits['eb_templates'] = $eb_templates
+  templates = elasticbeanstalk.templates.all
+  account_values['eb_templates'] = templates.length
+end
+if $max_vpc_securitygroups
+  my_groups = compute.describe_security_groups
+  vpc_map = Hash.new(0) 
+  my_groups[:body]['securityGroupInfo'].map {|group| vpc_map[group['vpcId']] += 1 }
+  account_limits['secgroups_per_vpc'] = $max_vpc_securitygroups
+  account_values['secgroups_per_vpc'] = vpc_map.values.max
+end
+if $max_cloudformation_stacks
+  cf_stacks = cloudformation.describe_stacks
+  account_limits['cloudformation_stacks'] = $max_cloudformation_stacks
+  account_values['cloudformation_stacks'] = cf_stacks.data[:body]['Stacks'].length
+end
+
 asg_limits = autoscaling_sdk.describe_account_limits()
 account_limits['autoscale_groups'] = asg_limits.data.max_number_of_auto_scaling_groups
 account_limits['launch_configs'] = asg_limits.data.max_number_of_launch_configurations
@@ -65,7 +104,6 @@ ec2_limits.data.account_attributes.each do |ec2_limit|
   account_limits[ec2_limit.attribute_name.tr('-', '_').sub('max_', '')] = ec2_limit.attribute_values[0].attribute_value
 end
 
-account_values = {}
 autoscalinggroup_list = autoscaling.groups.all
 account_values['autoscale_groups'] = autoscalinggroup_list.length
 autoscaling_launch_configs = autoscaling.configurations.all
@@ -85,11 +123,12 @@ account_limits.each do |limit, value|
 end
 
 account_values.each do |limit, value|
-  metricpath = "AWSlimits." + limit + ".value"
+  metricpath = "AWSlimits." + limit 
   metricvalue = value
   metrictimestamp = startTime
-  Sendit metricpath, metricvalue, metrictimestamp
-  $metricsSent += 1
+  Sendit "#{metricpath}.value", metricvalue, metrictimestamp
+  Sendit "#{metricpath}.used_percent", (metricvalue.to_i * 100 / account_limits[limit].to_i), metrictimestamp
+  $metricsSent += 2
 end
 
 $runEnd = Time.new.utc
